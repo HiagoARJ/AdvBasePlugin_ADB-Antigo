0527b11902a86568df877b341d6a16b3d274f02f
HiagoARJ <hiagoaraujo23@gmail.com>
2024-10-08 21:39:39 -0300

    Adição dos arquivos para criação dos plugins.

---
+++ NomePlugin/NomePlugin.cs
@@ -0,0 +1,40 @@
+﻿using AdvancedBot.Client;
+using AdvancedBot.Client.Commands;
+using AdvancedBot.Plugins;
+using AdvancedBot.Client.Handler;
+using System.Windows.Input;
+
+namespace PluginADV
+{
+    public class PluginADV : IPlugin
+    {
+        private PluginADVCommand cmdPlugin;
+
+        public void onClientConnect(MinecraftClient client)
+        {
+            if (client.CmdManager.GetCommand("plugin") == null)
+            {
+                cmdPlugin = new PluginADVCommand(client);
+                client.CmdManager.Commands.Add(new PluginADVCommand(client));
+            }
+        }
+
+        public void onReceiveChat(string chat, byte pos, MinecraftClient client)
+        { }
+
+        public void OnReceivePacket(ReadBuffer pkt, MinecraftClient client)
+        { }
+
+        public void onSendChat(string chat, MinecraftClient client)
+        { }
+
+        public void OnSendPacket(IPacket packet, MinecraftClient client)
+        { }
+
+        public void Tick()
+        { }
+
+        public void Unload()
+        { }
+    }
+}
---
+++ NomePlugin/NomePluginCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using AdvancedBot.Client.Map;
+using AdvancedBot.Client.Packets;
+using AdvancedBot.Client.Commands;
+
+namespace AdvancedBot.Client.Commands
+{
+    class PluginADVCommand : ICommand
+    {
+
+        private bool isToggled;
+        private readonly MinecraftClient client;
+
+        public PluginADVCommand(MinecraftClient cli)
+        : base(cli, "Plugin", "Descrição do Plugin", "plugin")
+        {
+            isToggled = false;
+            client = cli;
+        }
+
+        public override CommandResult Run(string alias, string[] args)
+        {
+
+            Toggle();
+            if (!isToggled)
+            {
+                Client.PrintToChat("§fPlugin: §c§lDesativado§f.");
+            }
+            else
+            {
+                Client.PrintToChat("§fPlugin: §a§lAtivado§f.");
+            }
+            return CommandResult.Success;
+        }
+
+        public override void Tick()
+        {
+            if (isToggled) // Execute apenas se ainda não tiver executado
+            {
+                //Local dos códigos de programação
+            }
+        }
+
+        private void Toggle()
+        {
+            isToggled = !isToggled;
+        }
+
+    }
+
+}
